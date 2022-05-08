cb0b19cdf5b2c899911411f10d12b47d6658c510
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-05-09 07:12:31 +0900

    minitest_reportersのセットアップ

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
+# gem 'minitest-reporters' setup
+require 'minitest/reporters'
+Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
