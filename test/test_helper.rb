0123a40bea539091b5da1a78fedfe878fd42f46a
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-05-09 07:20:44 +0900

    mini_testのセットアップ(seedをfixtureとして利用)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -5,12 +7,26 @@ require 'rails/test_help'
 # gem 'minitest-reporters' setup
 require 'minitest/reporters'
 Minitest::Reporters.use!
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+module ActiveSupport
+  class TestCase
+    # 並列テスト ... 複数のプロセスを分岐させテスト時間の短縮を行う機能
+    #
+    # プロセスが分岐した直後に呼び出し
+    parallelize_setup do |_worker|
+      # seedデータの読み込み
+      load "#{Rails.root}/db/seeds.rb"
+    end
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Run tests in parallel with specified workers
+    # 並列テストの有効化・無効化
+    # workers: プロセス数を渡す(2以上 => 有効、2未満 => 無効)
+    # number_of_processors => 使用しているマシンのコア数
+    # コンテナ上で nproc コマンドを使う事で確認可能
+    parallelize(workers: :number_of_processors)
 
-  # Add more helper methods to be used by all tests here...
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
 end
