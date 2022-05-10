36cc1abdd904a215dd79ddd233b4ab1168b98a04
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-05-11 08:14:17 +0900

    rubocop -Aにより修正

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,14 @@
-class Api::V1::UsersController < ApplicationController
-  def index
-    users = User.all
-    # as_json => ハッシュの形でJSONデータを返す { "id" => 1, "name" => "test", ...}
-    render json: users.as_json(only: %i[id name email created_at])
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # ユーザ一覧を返すコントローラ
+    class UsersController < ApplicationController
+      def index
+        users = User.all
+        # as_json => ハッシュの形でJSONデータを返す { "id" => 1, "name" => "test", ...}
+        render json: users.as_json(only: %i[id name email created_at])
+      end
+    end
   end
 end
