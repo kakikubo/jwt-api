41dfce6544eed7b43317a122c025450c5b5c0a6e
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-05-11 05:42:20 +0900

    バリデーション動作系のテストを書く

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,6 +52,52 @@ RSpec.describe User, type: :model do
       maxlength_msg = ["メールアドレスは#{max}文字以内で入力してください"]
       expect(user.errors.full_messages).to eq(maxlength_msg)
     end
+
+    it '正しい書式は保存できているか' do
+      ok_emails = %w[
+        A@EX.COM
+        a-_@e-x.c-o_m.j_p
+        a.a@ex.com
+        a@e.co.js
+        1.1@ex.com
+        a.a+a@ex.com
+      ]
+      ok_emails.each do |email|
+        user.email = email
+        # assert user.save
+        expect(user).to be_valid
+      end
+    end
+    it '間違った書式はエラーを吐いているか' do
+      ng_emails = %w[
+        aaa
+        a.ex.com
+        メール@ex.com
+        a~a@ex.com
+        a@|.com
+        a@ex.
+        .a@ex.com
+        a＠ex.com
+        Ａ@ex.com
+        a@?,com
+        １@ex.com
+        "a"@ex.com
+        a@ex@co.jp
+      ]
+      ng_emails.each do |email|
+        user.email = email
+        user.save
+        format_msg = ['メールアドレスは不正な値です']
+        expect(user.errors.full_messages).to eq(format_msg)
+      end
+    end
+
+    it 'email小文字化テスト' do
+      email = 'USER@EXAMPLE.COM'
+      user = User.new(email:)
+      user.save
+      expect(user.email).to eq('user@example.com')
+    end
   end
 end
 # rubocop:enable Metrics/BlockLength
