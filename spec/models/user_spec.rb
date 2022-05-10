a4df4f69b36032add3e726957c8ab1ce96684ebe
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-05-10 09:33:56 +0900

    テストを追加していく。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User, type: :model do
       user = User.new(email: 'test@example.com', password: 'password')
       user.save
       required_msg = ['名前を入力してください']
-      expect(required_msg).to eq(user.errors.full_messages)
+      expect(user.errors.full_messages).to eq(required_msg)
     end
 
     it '文字数制限は30文字まで' do
@@ -18,7 +18,38 @@ RSpec.describe User, type: :model do
       user.name = name
       user.save
       maxlength_msg = ['名前は30文字以内で入力してください']
-      expect(maxlength_msg).to eq(user.errors.full_messages)
+      expect(user.errors.full_messages).to eq(maxlength_msg)
+    end
+
+    it '30文字以内のユーザは保存できている' do
+      max = 30
+      user.name = 'あ' * max
+      # expect {
+      #   user.save
+      # }.to change(User, :count).by(1)
+
+      user.save
+      expect(user).to be_valid
+    end
+  end
+
+  describe "emailのバリデーション" do
+    let(:user) { active_user }
+    it "入力必須" do
+      user = User.new(name: 'test', password: 'password')
+      user.save
+      required_msg = ['メールアドレスを入力してください']
+      expect(user.errors.full_messages).to eq(required_msg)
+    end
+    it '文字数制限は255文字まで' do
+      max = 255
+      domain = "@example.com"
+      email = 'a' * (max + 1 - domain.length) + domain
+      assert max < email.length
+      user.email = email
+      user.save
+      maxlength_msg = ['メールアドレスは255文字以内で入力してください']
+      expect(user.errors.full_messages).to eq(maxlength_msg)
     end
   end
 end
