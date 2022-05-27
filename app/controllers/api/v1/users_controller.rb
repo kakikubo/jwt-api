c1472c8815754c763644436447c64b8276f296fd
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-05-28 07:43:05 +0900

    Add authenticate service module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,12 @@ module Api
   module V1
     # ユーザ一覧を返すコントローラ
     class UsersController < ApplicationController
+      before_action :authenticate_user
+
       def index
-        users = User.all
+        # users = User.all
         # as_json => ハッシュの形でJSONデータを返す { "id" => 1, "name" => "test", ...}
-        render json: users.as_json(only: %i[id name email created_at])
+        render json: current_user.as_json(only: %i[id name email created_at])
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  # Cookieを扱う
+  include ActionController::Cookies
+  # 認可を行う
+  include UserAuthenticateService
 end
