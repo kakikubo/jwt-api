46036a2ba2f09737eb21ce1c9f1c999a2037d5d2
kakikubo <t.kakikubo@fitsplus.co.jp>
2022-06-28 07:28:08 +0900

    update README

--- spec/requests/authentications_spec.rb
+++ spec/requests/authentications_spec.rb
@@ -45,36 +45,3 @@ RSpec.describe 'Authentications', type: :request do
     # end
   end
 end
-
-#   # 認証メソッドテスト
-#   test "authenticate_user_method" do
-#     login(@params)
-#     access_token = res_body[@access_token_key]
-
-#     # 有効なtokenでアクセスできているか
-#     projects_api(access_token)
-#     assert_response 200
-#     assert response.body.present?
-
-#     # 有効期限切れは401が返っているか
-#     travel_to (@access_lifetime.from_now) do
-#       # アクセス前のcookieは存在するか
-#       assert_not cookies[@session_key].blank?
-
-#       # レスポンスは想定通りか
-#       projects_api(access_token)
-#       assert_response 401
-#       assert_not response.body.present?
-
-#       # cookieは削除されているか
-#       assert cookies[@session_key].blank?
-#     end
-
-#     # 不正なtokenが投げられた場合
-#     invalid_token = "a." + access_token
-#     projects_api(invalid_token)
-
-#     assert_response 401
-#     assert_not response.body.present?
-#   end
-# end
